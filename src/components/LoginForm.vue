627342d91cb0423a6d72fcb5910701fcb4080a20
Denis Malashin <dmalashin@gmail.com>
2020-04-18 12:06:13 +0300

    add login form, change navbar

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="column is-one-third">
+    <form action="/" method="get">
+      <b-field>
+        <b-input placeholder="Email" type="email"></b-input>
+      </b-field>
+
+      <b-field>
+        <b-input placeholder="Password" type="password" min="6" max="20"></b-input>
+      </b-field>
+
+      <b-button tag="input" native-type="submit" class="is-submit" value="Sign in"></b-button>
+    </form>
+  </section>
+</template>
+
+
+<script>
+export default {
+  name: "LoginForm"
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="columns is-mobile is-centered">
+        <LoginForm />
+    </section>
+</template>
+
+
+<script>
+import LoginForm from '@/components/LoginForm'
+export default {
+    name: "Login",
+    components: {
+        LoginForm
+    }
+}
+</script>
