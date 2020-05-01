6b064a611c45994e989dcef527203ee3a11fd7e2
denis-md <dmalashin@gmail.com>
2020-05-01 12:43:33 +0300

    Initial dist subtree commit

---
+++ precache-manifest.741c9632ed196b4b6e911418934d81d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2b336b167d8c21268f",
+    "url": "/medium-like-app/css/app.1e8535e2.css"
+  },
+  {
+    "revision": "5f0ba9407824dc498b3a",
+    "url": "/medium-like-app/css/chunk-vendors.6bbededb.css"
+  },
+  {
+    "revision": "4f3d13e267c35151f7a735ee1c1bac1b",
+    "url": "/medium-like-app/index.html"
+  },
+  {
+    "revision": "1b2b336b167d8c21268f",
+    "url": "/medium-like-app/js/app.eff86a2b.js"
+  },
+  {
+    "revision": "5f0ba9407824dc498b3a",
+    "url": "/medium-like-app/js/chunk-vendors.af759098.js"
+  },
+  {
+    "revision": "a393cc1e88c88375d8c400e54cf3b70f",
+    "url": "/medium-like-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/medium-like-app/robots.txt"
+  }
+]);
